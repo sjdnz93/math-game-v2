61182e14d876d46250c5dea5aa32f789280c13d6
Simon Davis <s.davis@hotmail.co.nz>
2024-04-11 18:13:55 +0300

    multiplication game logic added

--- GameLogic/MultiplicationGameLogic.cs
+++ GameLogic/MultiplicationGameLogic.cs
@@ -4,7 +4,29 @@ public class MultiplicationGameLogic
 {
   public static void Launch()
   {
-    Console.WriteLine("MULTIPLICATION GAME!");
+    while (Status.questionsAsked < 10)
+    {
+      Console.Clear();
+      Console.WriteLine("MULTIPLICATION GAME!");
+
+      Random rnd = new Random();
+
+      int firstNumber = rnd.Next(0, 11);
+      int secondNumber = rnd.Next(0, 11);
+      int correctAnswer = firstNumber * secondNumber;
+
+      Console.WriteLine($"Your score is {Status.currentScore}\n");
+      Console.WriteLine($"Question {Status.questionsAsked + 1} of 10:");
+      Console.WriteLine($"{firstNumber} x {secondNumber} = ?");
+
+      bool inputIsValid = false;
+
+      Helpers.ValidateUserAnswer(inputIsValid, correctAnswer);
+
+    }
+
+    GameEngine.EndGame();
+
   }
 
 }
--- obj/Debug/net8.0/MathGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/MathGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+690007d2454d5f2e77ceb7005d03c9564cf53e0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+221d4946af85e2672264f84fb2c117a797f68c13")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
