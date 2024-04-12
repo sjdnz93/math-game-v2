3ba733eb79adbeb8a8fefef9ca9f6c3dc4680f88
Simon Davis <s.davis@hotmail.co.nz>
2024-04-12 11:45:46 +0300

    division game logic implemented

--- GameLogic/DivisionGameLogic.cs
+++ GameLogic/DivisionGameLogic.cs
@@ -4,7 +4,35 @@ public class DivisionGameLogic
 {
   public static void Launch()
   {
-    Console.WriteLine("DIVISION GAME!");
+    while (Status.questionsAsked < 10)
+    {
+      Console.Clear();
+      Console.WriteLine("DIVISION GAME!");
+
+      Random rnd = new Random();
+
+      float firstNumber = rnd.Next(1, 51);
+      float secondNumber = rnd.Next(2, 51);
+
+      while (firstNumber % secondNumber != 0 || secondNumber >= firstNumber) {
+        firstNumber = rnd.Next(1, 51);
+        secondNumber = rnd.Next(2, 51);
+      }
+
+      float correctAnswer = firstNumber / secondNumber;
+
+      Console.WriteLine($"Your score is {Status.currentScore}\n");
+      Console.WriteLine($"Question {Status.questionsAsked + 1} of 10:");
+      Console.WriteLine($"{firstNumber} / {secondNumber} = ?");
+
+      bool inputIsValid = false;
+
+      Helpers.ValidateUserAnswer(inputIsValid, (int)correctAnswer);
+
+    }
+
+    GameEngine.EndGame();
+
   }
 
 }
--- obj/Debug/net8.0/MathGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/MathGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+221d4946af85e2672264f84fb2c117a797f68c13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61182e14d876d46250c5dea5aa32f789280c13d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
